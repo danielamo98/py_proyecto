fb0b61255b0feda73fdcdf29ff41463700ffc6bd
danielamo98 <danielamejiaosorio15@gmail.com>
2025-04-05 10:04:18 -0500

    bb

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import csv
+
+
+
 
 # Configuración de la página
 st.set_page_config(   
@@ -27,3 +32,141 @@ st.markdown("""
 
 st.header("Solución")
 
+st.subheader("Actividad #1. Crea DataFrames desde diferentes fuentes")
+st.subheader("Objetivo de la actividad ")
+st.markdown("Familiarizarse con la creación de DataFrames en Pandas y mostrarlos usando Streamlit.")
+# ejercicio 1#
+st.header("Crea DataFrames desde Diccionario")
+st.write("DataFrame de Libros")
+
+code = '''
+datos = {"Titulo": ["Cien años de soledad", "El Alquimista", "El principito","Algebra"],
+         "Autor": ["Gabriel García Márquez", "Paulo Coelho", "Antoine de Saint-Exupéry","Aurelio Baldor"],
+         "Año de publicacion": ["1967 ","1988","1943","1941"],
+         "Genero": ["Ficcion", "Drama", "Fabula", "Ciencia"]}
+df = pd.DataFrame(datos)
+st.dataframe(df)
+    '''
+st.code(code, language="python")
+st.write("Resultado del codigo")
+
+datos = {"Titulo": ["Cien años de soledad", "El Alquimista", "El principito","Algebra"],
+         "Autor": ["Gabriel García Márquez", "Paulo Coelho", "Antoine de Saint-Exupéry","Aurelio Baldor"],
+         "Año de publicacion": ["1967 ","1988","1943","1941"],
+         "Genero": ["Ficcion", "Drama", "Fabula", "Ciencia"]}
+df = pd.DataFrame(datos)
+st.dataframe(df)
+
+# ejercicio 2 #
+st.header("Crea DataFrames desde Lista de diccionarios")
+st.write("DataFrame de Información de Ciudades")
+
+code ='''
+paises = [{"Nombre": "Medellin", "Poblacion": "2.533.424", "Pais": "Colombia"},
+          {"Nombre": "Paris", "Poblacion": "2 087 577", "Pais": "Francia"},
+          {"Nombre": "Caracas", "Poblacion": "7 826 165 ", "Pais": "Venezuela"}]
+df = pd.DataFrame(paises)
+st.dataframe(df)
+'''
+st.code(code, language="python")
+st.write("Resultado del codigo")
+
+paises = [{"Nombre": "Medellin", "Poblacion": "2.533.424", "Pais": "Colombia"},
+          {"Nombre": "Paris", "Poblacion": "2 087 577", "Pais": "Francia"},
+          {"Nombre": "Caracas", "Poblacion": "7 826 165 ", "Pais": "Venezuela"}]
+df = pd.DataFrame(paises)
+st.dataframe(df)
+
+# ejercicio 3 #
+
+st.header("Crea DataFrames desde Listas de Listas")
+st.write("DataFrame de Productos en Inventario")
+code ='''
+productos = [["Rubor", "25.000","3"],
+             ["Pestañina", "10.000","5"],
+             ["Labial", "8.000", "4"]]
+df = pd.DataFrame(productos, columns=["Nombre", "Pecio", "stock "])
+st.dataframe(df)
+'''
+st.code(code, language="python")
+st.write("Resultado del codigo")
+
+productos = [["Rubor", "25.000","3"],
+             ["Pestañina", "10.000","5"],
+             ["Labial", "8.000", "4"]]
+df = pd.DataFrame(productos, columns=["Nombre", "Pecio", "stock "])
+st.dataframe(df)
+
+# ejercicio 4 #
+
+st.header("Crea DataFrames desde Series")
+st.write("DataFrame de Datos de Personas")
+code ='''
+nombres = pd.Series(['Daniela', 'Maria', 'Andres'])
+edades = pd.Series([27, 30, 40])
+ciudades = pd.Series(['Cucuta', 'Roma', 'Bogota'])
+
+personas = pd.DataFrame({'Nombre': nombres, 'Edad': edades, 'Ciudad': ciudades})
+st.dataframe(personas)
+'''
+st.code(code, language="python")
+st.write("Resultado del codigo")
+
+
+nombres = pd.Series(['Daniela', 'Maria', 'Andres'])
+edades = pd.Series([27, 30, 40])
+ciudades = pd.Series(['Cucuta', 'Roma', 'Bogota'])
+
+personas = pd.DataFrame({'Nombre': nombres, 'Edad': edades, 'Ciudad': ciudades})
+st.dataframe(personas)
+
+# ejercicio 5 #
+
+st.header("Crea DataFrames desde Archivo CSV (local):")
+st.write("DataFrame de Datos desde CSV")
+code ='''
+column_names = ["Id", "Nombre", "Valor"]
+
+data = [
+    ["111", "Mango","2.000"],
+    ["222", "Pera", "1.000"],
+    ["333", "Fresa", "5.000"]
+]
+
+with open("data.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+
+    writer.writerow(column_names)
+
+    for row in data:
+        writer.writerow(row)
+
+df = pd.read_csv("data.csv")
+
+st.dataframe(df)
+
+'''
+st.code(code, language="python")
+st.write("Resultado del codigo")
+
+column_names = ["Id", "Nombre", "Valor"]
+
+data = [
+    ["111", "Mango","2.000"],
+    ["222", "Pera", "1.000"],
+    ["333", "Fresa", "5.000"]
+]
+
+with open("data.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+
+    writer.writerow(column_names)
+
+    for row in data:
+        writer.writerow(row)
+
+df = pd.read_csv("data.csv")
+
+st.dataframe(df)
+
+# ejercicio 6 #
